4f4b30272d52f9e213f7045281360ca0e53fbd50
FajarRamadhan27 <fajarramadhan220@gmail.com>
2023-03-11 13:21:19 +0700

    implement get contact

--- src/components/Table/ContactTable.jsx
+++ src/components/Table/ContactTable.jsx
@@ -20,32 +20,7 @@ import { visuallyHidden } from '@mui/utils';
 import DeleteIcon from '@mui/icons-material/Delete';
 import AddIcon from '@mui/icons-material/Add';
 import InputContactModal from '../Modal/InputContactModal';
-
-function createData(name, calories, fat, carbs, protein) {
-  return {
-    name,
-    calories,
-    fat,
-    carbs,
-    protein,
-  };
-}
-
-const rows = [
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Donut', 452, 25.0, 51, 4.9),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-  createData('Honeycomb', 408, 3.2, 87, 6.5),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Jelly Bean', 375, 0.0, 94, 0.0),
-  createData('KitKat', 518, 26.0, 65, 7.0),
-  createData('Lollipop', 392, 0.2, 98, 0.0),
-  createData('Marshmallow', 318, 0, 81, 2.0),
-  createData('Nougat', 360, 19.0, 9, 37.0),
-  createData('Oreo', 437, 18.0, 63, 4.0),
-];
+import { getContacts } from '../../utils/network';
 
 function descendingComparator(a, b, orderBy) {
   if (b[orderBy] < a[orderBy]) {
@@ -84,31 +59,25 @@ const headCells = [
     id: 'name',
     numeric: false,
     disablePadding: true,
-    label: 'Dessert (100g serving)',
+    label: 'Nama',
   },
   {
-    id: 'calories',
+    id: 'phone',
     numeric: true,
     disablePadding: false,
-    label: 'Calories',
+    label: 'Telepon',
   },
   {
-    id: 'fat',
+    id: 'address',
     numeric: true,
     disablePadding: false,
-    label: 'Fat (g)',
+    label: 'Alamat',
   },
   {
-    id: 'carbs',
+    id: 'title',
     numeric: true,
     disablePadding: false,
-    label: 'Carbs (g)',
-  },
-  {
-    id: 'protein',
-    numeric: true,
-    disablePadding: false,
-    label: 'Protein (g)',
+    label: 'Gelar',
   },
 ];
 
@@ -234,12 +203,20 @@ EnhancedTableToolbar.propTypes = {
 };
 
 export default function ContactTable() {
+  const [contacts, setConctact] = React.useState([]);
   const [order, setOrder] = React.useState('asc');
-  const [orderBy, setOrderBy] = React.useState('calories');
+  const [orderBy, setOrderBy] = React.useState('phone');
   const [selected, setSelected] = React.useState([]);
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(5);
 
+  React.useEffect(() => {
+    getContacts((result) => {
+      setConctact(result)
+    })
+  }, [])
+
+  
   const handleRequestSort = (event, property) => {
     const isAsc = orderBy === property && order === 'asc';
     setOrder(isAsc ? 'desc' : 'asc');
@@ -248,7 +225,7 @@ export default function ContactTable() {
 
   const handleSelectAllClick = (event) => {
     if (event.target.checked) {
-      const newSelected = rows.map((n) => n.name);
+      const newSelected = contacts.map((n) => n.name);
       setSelected(newSelected);
       return;
     }
@@ -288,7 +265,7 @@ export default function ContactTable() {
 
   // Avoid a layout jump when reaching the last page with empty rows.
   const emptyRows =
-    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
+    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - contacts.length) : 0;
 
   return (
     <Box sx={{ width: '100%' }}>
@@ -306,10 +283,10 @@ export default function ContactTable() {
               orderBy={orderBy}
               onSelectAllClick={handleSelectAllClick}
               onRequestSort={handleRequestSort}
-              rowCount={rows.length}
+              rowCount={contacts.length}
             />
             <TableBody>
-              {stableSort(rows, getComparator(order, orderBy))
+              {stableSort(contacts, getComparator(order, orderBy))
                 .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
                 .map((row, index) => {
                   const isItemSelected = isSelected(row.name);
@@ -342,10 +319,9 @@ export default function ContactTable() {
                       >
                         {row.name}
                       </TableCell>
-                      <TableCell align="right">{row.calories}</TableCell>
-                      <TableCell align="right">{row.fat}</TableCell>
-                      <TableCell align="right">{row.carbs}</TableCell>
-                      <TableCell align="right">{row.protein}</TableCell>
+                      <TableCell align="right">{row.phone}</TableCell>
+                      <TableCell align="right">{row.address}</TableCell>
+                      <TableCell align="right">{row.title}</TableCell>
                     </TableRow>
                   );
                 })}
@@ -364,7 +340,7 @@ export default function ContactTable() {
         <TablePagination
           rowsPerPageOptions={[5, 10, 25]}
           component="div"
-          count={rows.length}
+          count={contacts.length}
           rowsPerPage={rowsPerPage}
           page={page}
           onPageChange={handleChangePage}
--- src/utils/network/lib/Contact.js
+++ src/utils/network/lib/Contact.js
@@ -4,6 +4,10 @@ export function getContacts(setState) {
   axiosClient.get('/register_notif')
     .then(res => {
       const { data } = res.data
-      setState(data)
+      const formattedData = data.map((originalData) => {
+        const { user_name, user_phone, user_address, user_title, ...rest} = originalData
+        return { name: user_name, phone: user_phone, address: user_address, title: user_title, ...rest }
+      })
+      setState(formattedData)
     })
 }
